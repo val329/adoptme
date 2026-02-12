c709b3505a4d8f983313c1e9d5f56f3cde698356
val329 <143243484+val329@users.noreply.github.com>
2026-02-11 21:13:51 -0800

    Adding js file
    Basic logic using fetch() API for displaying one picture on page load

---
+++ app.js
@@ -0,0 +1,48 @@
+const URL = `https://api.thecatapi.com/v1/images/search?limit=1`;
+const API_KEY = "DEMO-API-KEY";
+
+fetch(URL,{headers: {
+    'x-api-key': API_KEY
+    }})
+
+    .then((response) => {
+        return response.json();
+    })
+
+    .then((data) => {
+        let imagesData = data;
+        imagesData.map(function(imageData) {
+
+            let image = document.createElement('img');
+            //use the url from the image object
+            image.src = `${imageData.url}`;
+            image.width = `${imageData.width}`;
+            image.height = `${imageData.height}`;
+            
+            // show_image(image.src,image.width,image.height);  
+            document.querySelector('.imageContainer').appendChild(image);  
+
+        })
+
+    })
+
+
+    .catch(function(error) {
+        console.log(error);
+    });
+
+    
+// function show_image(src, width, height) {
+//     // Create a new image element
+//     let img = document.createElement("img");
+
+//     // Set the source, width, height attributes
+//     img.src = src;
+//     img.width = width;
+//     img.height = height;
+
+//     // Append the image element to the image container
+//     // document.body.appendChild(img);
+//     document.querySelector('.imageContainer').appendChild(img);
+//     // document.getElementById("imageContainer").appendChild(img);
+// };
